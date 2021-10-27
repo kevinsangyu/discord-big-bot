ce925120d172ec33a4fc5a40d3db7316fde49caf
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2021-10-27 21:31:00 +1100

    new translate command
    using naver's papago API, I've made a simple [some language] to english translation command. I'll make a whole separate cog for translation, but this is just a start.

--- Discord_Big_Bot.py
+++ Discord_Big_Bot.py
@@ -7,6 +7,10 @@ import wikiCog
 import moneyCog
 import os
 import Botkeys
+import urllib.request
+import urllib.parse
+import json
+from urllib.error import HTTPError
 
 try:
     os.mkdir("logs")
@@ -19,7 +23,7 @@ except FileExistsError:
 
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix='Kevin ', intents = intents)
+client = commands.Bot(command_prefix='Kevin ', intents=intents)
 musicCog.setup(client)
 wikiCog.setup(client)
 moneyCog.setup(client)
@@ -441,6 +445,44 @@ async def poll(ctx, *, question):
     logger("Poll called with prompt: " + prompt + ", and options: " + str(options))
     for i in range(0, len(options)):
         await msg.add_reaction(number_emojis[i])
+
+
+@client.command(description="Translates a given string to english. Only supports 13 languages:\n"
+                              "korean, english, japanese, traditional chinese, simplified chinese, vietnamese, "
+                              "indonesian, thai, german, russian, spanish, italian and french.")
+async def translate(ctx, *, sentence):
+    keys = Botkeys.get_keys()
+    data = "query=" + urllib.parse.quote(sentence)
+    request = urllib.request.Request("https://openapi.naver.com/v1/papago/detectLangs")
+    request.add_header("X-Naver-Client-Id", keys["translate_id"])
+    request.add_header("X-Naver-Client-Secret", keys["translate_secret"])
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    if rescode == 200:
+        response_body = response.read()
+        srclang = json.loads(response_body.decode('utf-8'))["langCode"]
+        logger("Translate command: Given sentence is in: " + srclang)
+    else:
+        await ctx.send("Error Code:" + rescode)
+        logger("Error Code: " + rescode)
+        return
+    data = "source=" + srclang + "&target=en&text=" + urllib.parse.quote(sentence)
+    request = urllib.request.Request("https://openapi.naver.com/v1/papago/n2mt")
+    request.add_header("X-Naver-Client-Id", keys["translate_id"])
+    request.add_header("X-Naver-Client-Secret", keys["translate_secret"])
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    if rescode == 200:
+        response_body = response.read()
+        output = response_body.decode('utf-8')
+        output = json.loads(output)
+        await ctx.send(output["message"]["result"]["srcLangType"] + " to " +
+                       output["message"]["result"]["tarLangType"] + ": " + output["message"]["result"]["translatedText"])
+        logger(output["message"]["result"]["srcLangType"] + " to " + output["message"]["result"]["tarLangType"])
+    else:
+        await ctx.send("Error Code:" + rescode)
+        logger("Error Code: " + rescode)
+        return
 
 
 client.run(botkeys["BotToken"])
